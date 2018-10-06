fac4b872ffcfec3c068550d660a42f5ea1dc2792
Forkfolio <forkfolioproject@gmail.com>
2018-10-07 00:37:24 +0200

    move video to the left, enable fullscreen

--- src/views/common/HelpCard.jsx
+++ src/views/common/HelpCard.jsx
@@ -16,6 +16,10 @@ class HelpCard extends Component {
           content={
             <Grid fluid>
               <Row>
+                <Col md={6}>
+                  <iframe title="Getting started with ForkFolio" width="630" height="394" src="https://www.useloom.com/embed/f4f07be58c954532b3d3d9e62cde3dfd" frameBorder="0" webkitallowfullscreen mozallowfullscreen allowFullScreen></iframe>
+                </Col>
+
                 <Col md={3}>
                   <p>Your portfolio is similar to bank account - you need to deposit some funds to be able to trade one asset for another. Go to Funding -> Add Funding to deposit crypto or fiat currencies to portfolio.</p>
                   <p>If you don't know what your past trades are, you can use deposits to add final balance of your assets, and track your portfolio from that moment on.</p>
@@ -24,9 +28,6 @@ class HelpCard extends Component {
                 <Col md={3}>
                   
                   <p><i className={"fa fa-exclamation-triangle"} /> Important: don't forget to save your changes as <b><u>we don't have access</u></b> to any of your data. Go to Manage -> Save to save all changes you've made.  </p>
-                </Col>
-                <Col md={6}>
-                <iframe width="630" height="394" src="https://www.useloom.com/embed/f4f07be58c954532b3d3d9e62cde3dfd" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
                 </Col>
               </Row>
               <Row>
