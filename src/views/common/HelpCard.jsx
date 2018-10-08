e87ab43fd5488d144c9bc90794465f9227e56ee7
Forkfolio <forkfolioproject@gmail.com>
2018-10-09 01:19:25 +0200

    updated getting started

--- src/views/common/HelpCard.jsx
+++ src/views/common/HelpCard.jsx
@@ -16,18 +16,25 @@ class HelpCard extends Component {
           content={
             <Grid fluid>
               <Row>
-                <Col md={6}>
-                  <iframe title="Getting started with ForkFolio" width="630" height="394" src="https://www.useloom.com/embed/f4f07be58c954532b3d3d9e62cde3dfd" frameBorder="0" webkitallowfullscreen mozallowfullscreen allowFullScreen></iframe>
-                </Col>
 
-                <Col md={3}>
-                  <p>Your portfolio is similar to a bank account - you need to deposit some funds to be able to trade one asset for another. Go to Funding -> Add Funding to deposit crypto or fiat currencies to portfolio.</p>
-                  <p>If you don't know what your past trades are, you can use deposits to add final balance of your assets, and track your portfolio from that moment on.</p>
-                  <p>If you have your past trades, you can go to Trades -> Add Trades to add your past trades. Now you can analyze how much are your trades profitable, and get insight into your trading strategy.</p>
+
+                <Col lg={5} md={3} sm={0} xs={0}>
+                  <p>Your portfolio is similar to a bank account - you need to deposit an asset to portfolio to be able to trade that asset for another one. </p>
+                  <h5>Adding your first deposit</h5> 
+                  <ol>
+                    <li>navigate to the <a href="#/funding">Funding</a> page,</li>
+                    <li>click on the Add Funding button,</li>
+                    <li>add your first deposit.</li>
+                  </ol>
+                  <h5>Adding your first trade</h5> 
+                  <ol>
+                    <li>navigate to the <a href="#/trades">Trades</a> page,</li>
+                    <li>click on the Add Trade button,</li>
+                    <li>add your first trade.</li>
+                  </ol>                  
                 </Col>
-                <Col md={3}>
-                  
-                  <p><i className={"fa fa-exclamation-triangle"} /> Important: don't forget to save your changes as <b><u>we don't have access</u></b> to any of your data. Go to Manage -> Save to save all changes you've made.  </p>
+                <Col lg={7} md={9} sm={12} xs={12} >
+                  <iframe title="Getting started with ForkFolio" width="600" height="370" src="https://www.useloom.com/embed/f4f07be58c954532b3d3d9e62cde3dfd" frameBorder="0" webkitallowfullscreen mozallowfullscreen allowFullScreen></iframe>
                 </Col>
               </Row>
               <Row>
