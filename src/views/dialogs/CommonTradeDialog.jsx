de5f10b15e71c7d4cc1a8dcbcefcd9d9e19d0de0
Forkfolio <forkfolioproject@gmail.com>
2018-10-13 01:47:56 +0200

    improving add/edit trade dialog #2

--- src/views/dialogs/CommonTradeDialog.jsx
+++ src/views/dialogs/CommonTradeDialog.jsx
@@ -12,7 +12,10 @@ import {
   Form,
   FormGroup,
   FormControl,
-  ControlLabel
+  ControlLabel,
+  Nav,
+  NavItem,
+  Tab
 } from "react-bootstrap";
 
 import Button from "components/CustomButton/CustomButton.jsx";
@@ -127,7 +130,132 @@ class CommonTradeDialog extends Component {
     return current.isBefore(Datetime.moment());
   };
 
+  getBuyFormGroup(name, placeholder) {
+    return (
+      <FormGroup controlId="formHorizontalNumber">
+      <Col componentClass={ControlLabel} sm={3} smOffset={0}>
+        {name}
+      </Col>
+      <Col sm={5}>
+        <FormControl
+          placeholder={placeholder}
+          type="number"
+          min={0}
+          name="buyAmount"
+          value={this.state.buyAmount}
+          onChange={event => {
+            this.setState({
+              buyAmount: event.target.value
+            });
+            this.isValidBuyAmount(event.target.value);
+          }}
+        />
+        {this.state.buyAmountError}
+      </Col>
+      <Col sm={3}>
+        <Select
+          placeholder="Currency"
+          name="buyCurrency"
+          value={this.state.buyCurrency}
+          options={this.state.buyCurrencies}
+          onChange={value => {
+            this.setState({ buyCurrency: value });
+            this.isValidBuyCurrency(value);
+          }}
+        />
+        {this.state.buyCurrencyError}
+      </Col>
+    </FormGroup>
+    );
+  }
+
+  getSellFormGroup(name, placeholder) {
+    return (
+      <FormGroup controlId="formHorizontalNumber">
+      <Col componentClass={ControlLabel} sm={3} smOffset={0}>
+        {name}
+      </Col>
+      <Col sm={5}>
+        <FormControl
+          placeholder={placeholder}
+          type="number"
+          name="sellAmount"
+          min={0}
+          value={this.state.sellAmount}
+          onChange={event => {
+            this.setState({
+              sellAmount: event.target.value
+            });
+            this.isValidSellAmount(event.target.value);
+          }}
+        />
+        {this.state.sellAmountError}
+      </Col>
+      <Col sm={3}>
+        <Select
+          placeholder="Currency"
+          name="sellCurrency"
+          value={this.state.sellCurrency}
+          options={this.state.sellCurrencies}
+          onChange={value => {
+            this.setState({ sellCurrency: value });
+            this.isValidSellCurrency(value);
+          }}
+        />
+        {this.state.sellCurrencyError}
+      </Col>
+    </FormGroup>
+    );
+  }
+
+  getDateFormGroup() {
+    return (
+      <FormGroup controlId="formHorizontalNumber">
+      <Col componentClass={ControlLabel} sm={3} smOffset={0}>
+        Date
+      </Col>
+      <Col sm={5}>
+        <Datetime
+          isValidDate={this.isEnabledDate}
+          timeFormat={false}
+          inputProps={{ placeholder: "Click to select a date" }}
+          value={this.state.date}
+          onChange={event => {
+            this.setState({ date: (typeof event === "string") ? null : event.toDate()});
+            this.isValidDate((typeof event === "string") ? null : event.toDate());
+          }}
+        />
+        {this.state.dateError}
+      </Col>
+    </FormGroup>
+    );
+  }
+
+  getCommentFormGroup() {
+    return (
+      <FormGroup controlId="formHorizontalURL">
+      <Col componentClass={ControlLabel} sm={3} smOffset={0}>
+        Comment
+      </Col>
+      <Col sm={5}>
+        <FormControl
+          type="text"
+          name="comment"
+          value={this.state.comment}
+          onChange={event => {
+            this.setState({ comment: event.target.value });
+          }}
+        />
+      </Col>
+    </FormGroup>
+    );
+  }
+
   render() {
+    let defaultActiveKey = "buy";
+    if(this.props.editedTransaction != null && !this.props.editedTransaction.isBuy) {
+      defaultActiveKey = "sell";
+    }
     return (
       <div className="main-content">
         <Grid fluid>
@@ -141,129 +269,36 @@ class CommonTradeDialog extends Component {
                   <Modal.Title>{this.state.title}</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
-                  <Form horizontal>  
-
-
-                    <FormGroup controlId="formHorizontalNumber">
-                      <Col componentClass={ControlLabel} sm={4} smOffset={0}>
-                        Buy amount
+                  <Tab.Container id="tabs-with-dropdown" defaultActiveKey={defaultActiveKey}>
+                    <Row className="clearfix">
+                      <Col sm={12}>
+                        <Nav bsStyle="tabs">
+                          <NavItem eventKey="buy">Buy</NavItem>
+                          <NavItem eventKey="sell">Sell</NavItem>
+                        </Nav>
                       </Col>
-                      <Col sm={6}>
-                        <FormControl
-                          type="number"
-                          min={0}
-                          name="buyAmount"
-                          value={this.state.buyAmount}
-                          onChange={event => {
-                            this.setState({
-                              buyAmount: event.target.value
-                            });
-                            this.isValidBuyAmount(event.target.value);
-                          }}
-                        />
-                        {this.state.buyAmountError}
+                      <Col sm={12}>
+                        <Tab.Content animation>
+                          <Tab.Pane eventKey="buy">
+                            <Form horizontal>  
+                              {this.getBuyFormGroup("Buy amount", "How much did you buy?")}
+                              {this.getSellFormGroup("Cost", "How much did it cost you?")}
+                              {this.getDateFormGroup()}
+                              {this.getCommentFormGroup()}
+                            </Form>
+                          </Tab.Pane>
+                          <Tab.Pane eventKey="sell">
+                            <Form horizontal>  
+                              {this.getSellFormGroup("Sell amount", "How much did you sell?")}
+                              {this.getBuyFormGroup("Proceeds", "How much did you receive?")}
+                              {this.getDateFormGroup()}
+                              {this.getCommentFormGroup()}
+                            </Form>
+                          </Tab.Pane>
+                        </Tab.Content>
                       </Col>
-                    </FormGroup>
-
-                    <FormGroup controlId="formHorizontalNumber">
-                      <Col componentClass={ControlLabel} sm={4} smOffset={0}>
-                        Buy currency
-                      </Col>
-                      <Col sm={6}>
-                        <Select
-                          placeholder="Type or select currency"
-                          name="buyCurrency"
-                          value={this.state.buyCurrency}
-                          options={this.state.buyCurrencies}
-                          onChange={value => {
-                            this.setState({ buyCurrency: value });
-                            this.isValidBuyCurrency(value);
-                          }}
-                        />
-                        {this.state.buyCurrencyError}
-                      </Col>
-                    </FormGroup>
-
-
-                    <FormGroup controlId="formHorizontalNumber">
-                      <Col componentClass={ControlLabel} sm={4} smOffset={0}>
-                        Sell amount
-                      </Col>
-                      <Col sm={6}>
-                        <FormControl
-                          type="number"
-                          name="sellAmount"
-                          min={0}
-                          value={this.state.sellAmount}
-                          onChange={event => {
-                            this.setState({
-                              sellAmount: event.target.value
-                            });
-                            this.isValidSellAmount(event.target.value);
-                          }}
-                        />
-                        {this.state.sellAmountError}
-                      </Col>
-                    </FormGroup>
-
-                    <FormGroup controlId="formHorizontalNumber">
-                      <Col componentClass={ControlLabel} sm={4} smOffset={0}>
-                        Sell currency
-                      </Col>
-                      <Col sm={6}>
-                        <Select
-                          placeholder="Type or select currency"
-                          name="sellCurrency"
-                          value={this.state.sellCurrency}
-                          options={this.state.sellCurrencies}
-                          onChange={value => {
-                            this.setState({ sellCurrency: value });
-                            this.isValidSellCurrency(value);
-                          }}
-                        />
-                        {this.state.sellCurrencyError}
-                      </Col>
-                    </FormGroup>
-
-
-                    <FormGroup controlId="formHorizontalNumber">
-                      <Col componentClass={ControlLabel} sm={4} smOffset={0}>
-                        Date
-                      </Col>
-                      <Col sm={6}>
-                        <Datetime
-                          isValidDate={this.isEnabledDate}
-                          timeFormat={false}
-                          inputProps={{ placeholder: "Click to select a date" }}
-                          value={this.state.date}
-                          onChange={event => {
-                            this.setState({ date: (typeof event === "string") ? null : event.toDate()});
-                            this.isValidDate((typeof event === "string") ? null : event.toDate());
-                          }}
-                        />
-                        {this.state.dateError}
-                      </Col>
-                    </FormGroup>
-
-
-                    <FormGroup controlId="formHorizontalURL">
-                      <Col componentClass={ControlLabel} sm={2} smOffset={2}>
-                        Comment
-                      </Col>
-                      <Col sm={6}>
-                        <FormControl
-                          type="text"
-                          name="comment"
-                          value={this.state.comment}
-                          onChange={event => {
-                            this.setState({ comment: event.target.value });
-                          }}
-                        />
-                      </Col>
-                    </FormGroup>
-
-
-                  </Form>
+                    </Row>
+                  </Tab.Container>
                 </Modal.Body>
                 <Modal.Footer>
                   <Button
@@ -277,7 +312,7 @@ class CommonTradeDialog extends Component {
                     fill
                     wd
                     type="submit"
-                    onClick={/*() => */this.handleSaveButtonClick.bind(this)}
+                    onClick={this.handleSaveButtonClick.bind(this)}
                   >
                     Save
                   </Button>
