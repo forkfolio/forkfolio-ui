f8e42212c5ec2953138b8ef6a01294cf410ea9b5
Forkfolio <forkfolioproject@gmail.com>
2018-11-07 03:22:47 +0100

    feature - demo - update build script

--- src/tools/copyBuild.js
+++ src/tools/copyBuild.js
@@ -1,11 +1,22 @@
 const fs = require('fs-extra');
 const chalk = require('chalk');
 
-fs.emptyDirSync('docs/app');
+try {
+  fs.emptyDirSync('docs/app');
+  fs.emptyDirSync('docs/demo');
 
-fs.copy('build', 'docs/app', err => {
-  if (err) {
-    return console.log(err); 
-  }
-  console.log(chalk.green("Build successful. All files are ready for commit and push!"));
-});
+  fs.copySync('build', 'docs/app');
+  fs.copySync('build/config-app/appversion.json', 'docs/app/appversion.json');
+  fs.copySync('build', 'docs/demo');
+  fs.copySync('build/config-demo/appversion.json', 'docs/demo/appversion.json');
+
+  fs.removeSync('docs/app/config-app');
+  fs.removeSync('docs/app/config-demo');
+  fs.removeSync('docs/demo/config-app');
+  fs.removeSync('docs/demo/config-demo');
+
+  console.log(chalk.green("Build of app and demo successful. All files are ready for commit and push!"));
+} catch (err) {
+  console.error(err);
+}
+  
