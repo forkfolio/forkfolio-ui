2f0ab3ec425ad065b411f8157dd48946cd351abd
Forkfolio <forkfolioproject@gmail.com>
2018-11-27 03:43:05 +0100

    simpler help

--- src/views/common/HelpCard.jsx
+++ src/views/common/HelpCard.jsx
@@ -14,19 +14,11 @@ class HelpCard extends Component {
             <Grid fluid>
               <Row>
                 <Col lg={5} md={3} sm={0} xs={0}>
-                  <p>Your portfolio is similar to a bank account - you need to deposit an asset to portfolio to be able to trade that asset for another one. </p>
-                  <h5>Adding your first deposit</h5> 
-                  <ol>
-                    <li>navigate to the <a href="#/funding">Funding</a> page,</li>
-                    <li>click on the Add Funding button,</li>
-                    <li>add your first deposit.</li>
-                  </ol>
-                  <h5>Adding your first trade</h5> 
-                  <ol>
-                    <li>navigate to the <a href="#/trades">Trades</a> page,</li>
-                    <li>click on the Add Trade button,</li>
-                    <li>add your first trade.</li>
-                  </ol>                  
+                  <p>Welcome to Forkfolio app, a private-by-design crypto portfolio manager you can use to watch live crypto prices, track your trades, and get insights into your trading performance. </p><p>If you haven't seen Forkfolio in action, visit <a href="https://forkfol.io/demo/">demo app</a> loaded with public portfolio that showcases all features.</p>
+                  <h5>Quick start</h5> 
+                  <p>Your portfolio is like a bank account - first you need to fund your portfolio with tokens to be able to trade them for another one. </p>
+                  <p>We recommend that you start by adding deposits of all tokens you own on the <a href="#/funding">Funding</a> page. </p>
+                  <p>Click on a Play button to watch a short video that will show you how to add deposits and trades. </p>
                 </Col>
                 <Col lg={7} md={9} sm={12} xs={12} >
                   <iframe title="Getting started with ForkFolio" width="600" height="370" src="https://www.useloom.com/embed/87407d8535bd43b7b5c8a67da8c99d77" frameBorder="0" webkitallowfullscreen mozallowfullscreen allowFullScreen></iframe>
