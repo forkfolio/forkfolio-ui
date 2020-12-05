e2fd77b2d2cdb21f45ca231c8737dcbae7a37498
forkfolio <43525661+forkfolio@users.noreply.github.com>
2020-12-05 17:11:37 +0100

    debug 2.0.8

--- src/model/ResModel.js
+++ src/model/ResModel.js
@@ -23,7 +23,9 @@ export default class ResModel {
 
     if(counter === this.usd) {
       console.log(1)
+      console.log(base);
       console.log(this.recentTickers);
+      console.log(this.recentTickers.get(base))
       // counter usd, there is recent base
       if(this.recentTickers.get(base) != null) {
         console.log("returning " + base + ": " +  this.recentTickers.get(base).price)
