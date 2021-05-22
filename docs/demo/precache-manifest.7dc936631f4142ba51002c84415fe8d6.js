73a8c50be872d017d0a20c52650b5d3f70f478f5
forkfolio <43525661+forkfolio@users.noreply.github.com>
2021-05-22 10:17:32 +0200

    build 3.0.0

---
+++ docs/demo/precache-manifest.7dc936631f4142ba51002c84415fe8d6.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/app/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "f8d5dd7187d6dcbd4022",
+    "url": "/app/static/css/main.49a7a44d.chunk.css"
+  },
+  {
+    "revision": "127917d16c2343d0c8fa",
+    "url": "/app/static/js/runtime~main.d465ad9e.js"
+  },
+  {
+    "revision": "dedf26989fdd78c33cb9ae664a604d6c",
+    "url": "/app/static/media/Pe-icon-7-stroke.dedf2698.svg"
+  },
+  {
+    "revision": "3f63bc8556b826a4a255",
+    "url": "/app/static/js/2.bc0c119d.chunk.js"
+  },
+  {
+    "revision": "ef9c8d65872c022182a94ffa984603d3",
+    "url": "/app/static/media/full-screen-image-3.ef9c8d65.jpg"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/app/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/app/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "f8d5dd7187d6dcbd4022",
+    "url": "/app/static/js/main.8b17ad63.chunk.js"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/app/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/app/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "71394c0c7ad6c1e7d5c77e8ac292fba5",
+    "url": "/app/static/media/Pe-icon-7-stroke.71394c0c.eot"
+  },
+  {
+    "revision": "b38ef310874bdd008ac14ef3db939032",
+    "url": "/app/static/media/Pe-icon-7-stroke.b38ef310.woff"
+  },
+  {
+    "revision": "01798bc13e33afc36a52f2826638d386",
+    "url": "/app/static/media/Pe-icon-7-stroke.01798bc1.ttf"
+  },
+  {
+    "revision": "3f63bc8556b826a4a255",
+    "url": "/app/static/css/2.495718e2.chunk.css"
+  },
+  {
+    "revision": "f04373f3c10b596af65c6c70ebf44aad",
+    "url": "/app/index.html"
+  }
+];
